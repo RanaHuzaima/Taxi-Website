16677ed18e8f5b7466f63a91a368427a7c67f9a8
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-21 19:18:21 +0500

    Make Header & Home Section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <div className="container flex flex-col mx-auto bg-white">
+        <div className="relative flex flex-wrap items-center justify-between w-full bg-white group py-7 shrink-0">
+          <div>
+            <img
+              className="lg:h-20 h-16 cursor-pointer"
+              src="https://umrah-taxi.com/assets/umrah-taxi1-1c57b6fa08daec5a400f7ce309cda26cff87f87955037c4c0aa0643e031b9a92.png"
+            />
+          </div>
+          <div className="items-center justify-between hidden gap-12 text-black md:flex">
+            {/* Add your NavLink components here */}
+            <NavLink
+              className="font-bold lg:text-2xl text-dark-grey-700 hover:text-dark-grey-900"
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className="lg:text-2xl font-bold text-dark-grey-700 hover:text-dark-grey-900 "
+              exact
+              activeStyle={{ color: "red" }}
+              to="/pricing"
+            >
+              Pricing
+            </NavLink>
+            <NavLink
+              className="lg:text-2xl font-bold text-dark-grey-700 hover:text-dark-grey-900"
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className="lg:text-2xl font-bold text-dark-grey-700 hover:text-dark-grey-900"
+              to="/contact"
+            >
+              Contact Us
+            </NavLink>
+          </div>
+          <div className="items-center hidden gap-8 md:flex">
+            <button className="flex items-center px-4 py-2 lg:text-2xl font-bold rounded-xl  bg-yellow-400 hover:bg-yellow-600 text-white transition duration-300">
+              Contact Us
+            </button>
+          </div>
+          <button onClick={handleButtonClick} className="flex md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M3 8H21C21.2652 8 21.5196 7.89464 21.7071 7.70711C21.8946 7.51957 22 7.26522 22 7C22 6.73478 21.8946 6.48043 21.7071 6.29289C21.5196 6.10536 21.2652 6 21 6H3C2.73478 6 2.48043 6.10536 2.29289 6.29289C2.10536 6.48043 2 6.73478 2 7C2 7.26522 2.10536 7.51957 2.29289 7.70711C2.48043 7.89464 2.73478 8 3 8ZM21 16H3C2.73478 16 2.48043 16.1054 2.29289 16.2929C2.10536 16.4804 2 16.7348 2 17C2 17.2652 2.10536 17.5196 2.29289 17.7071C2.48043 17.8946 2.73478 18 3 18H21C21.2652 18 21.5196 17.8946 21.7071 17.7071C21.8946 17.5196 22 17.2652 22 17C22 16.7348 21.8946 16.4804 21.7071 16.2929C21.5196 16.1054 21.2652 16 21 16ZM21 11H3C2.73478 11 2.48043 11.1054 2.29289 11.2929C2.10536 11.4804 2 11.7348 2 12C2 12.2652 2.10536 12.5196 2.29289 12.7071C2.48043 12.8946 2.73478 13 3 13H21C21.2652 13 21.5196 12.8946 21.7071 12.7071C21.8946 12.5196 22 12.2652 22 12C22 11.7348 21.8946 11.4804 21.7071 11.2929C21.5196 11.1054 21.2652 11 21 11Z"
+                fill="black"
+              ></path>
+            </svg>
+          </button>
+          <div
+            className={`absolute flex md:hidden transition-all duration-300 ease-in-out flex-col shadow-main text-center justify-center w-full gap-3 overflow-hidden bg-white max-h-0 ${
+              menuOpen ? "py-4 px-4 rounded-2xl max-h-64" : ""
+            } top-full border `}
+          >
+            {/* Add your NavLink components here */}
+            <NavLink
+              className="font-bold text-dark-grey-700 hover:text-dark-grey-900 border rounded-lg"
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className=" font-bold text-dark-grey-700 hover:text-dark-grey-900 border rounded-lg"
+              to="/pricing"
+            >
+              Pricing
+            </NavLink>
+            <NavLink
+              className=" font-bold text-dark-grey-700 hover:text-dark-grey-900 border rounded-lg"
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className=" font-bold text-dark-grey-700 hover:text-dark-grey-900 border rounded-lg"
+              to="/contact"
+            >
+              Contact Us
+            </NavLink>
+            <button className="flex items-center px-4 py-2 text-sm font-bold rounded-xl  bg-yellow-400 hover:bg-yellow-600 text-white transition duration-300">
+              Contact Us
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
-  )
-}
+    <>
+      <div className="container flex flex-col mx-auto bg-white">
+        <div className="grid w-full grid-cols-1 my-auto mt-12 mb-8 md:grid-cols-2 xl:gap-14 md:gap-5">
+          <div className="flex flex-col justify-center col-span-1 text-center lg:text-start">
+            <div className="flex items-center justify-center mb-4 lg:justify-normal">
+              <img
+                className="h-5"
+                src="https://icones.pro/wp-content/uploads/2021/02/icone-de-localisation-jaune.png"
+                alt="logo"
+              />
+              <h4 className="ml-2 text-sm font-bold tracking-widest text-yellow-400 uppercase">
+                online taxi service provides
+              </h4>
+            </div>
+            <h1 className="mb-8 text-4xl font-extrabold leading-tight lg:text-6xl">
+              Lets Book Your Umrah Taxi Instantly
+            </h1>
+            <p className="mb-6 text-base font-normal leading-7 lg:w-3/4 text-grey-900">
+              Experience seamless and convenient Umrah pilgrimage with our
+              top-notch Umrah Taxi Service. We offer reliable transportation,
+              experienced drivers, and exceptional customer service to ensure a
+              worry-free journey.
+            </p>
+            <div className="flex flex-col items-center gap-4 lg:flex-row">
+              <button className="flex items-center py-4 text-lg font-bold text-white px-7 bg-yellow-400 hover:bg-yellow-600  transition duration-300 rounded-xl">
+                Book Now
+              </button>
+              <button className="flex items-center py-4 text-lg font-medium px-7 text-dark-grey-700 hover:text-dark-grey-900 transition duration-300 rounded-xl border hover:bg-yellow-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5 mr-2"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                Call Now
+              </button>
+            </div>
+          </div>
+          <div className="items-center justify-end hidden col-span-1 md:flex">
+            <img
+              className="w-4/5 rounded-md"
+              src="https://media.istockphoto.com/id/492362277/photo/3d-yellow-taxi.jpg?s=612x612&w=0&k=20&c=RXoWaS8t0UrqGN0cFxrbLDROw_bThdCrh-lSYjWU5L0="
+              alt="header image"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
