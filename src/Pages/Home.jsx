b25a5e358e0760e9343d0d06abb56c93b8ce8ba0
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-22 18:36:10 +0500

    Add Hyundai Staria car in fleet & pick section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -103,6 +103,13 @@ const Home = () => {
               />
               <span>H1 HYUNDAI 7 Seater Van</span>
             </div>
+            <div className="flex flex-col items-center">
+              <img
+                src="https://i.ibb.co/7CNgB2D/hyundai-staria-63d1fe79af681-prev-ui-1.png"
+                alt="pick-up-icon"
+              />
+              <span className="mt-1">HYUNDAI Staria 9 Seater Van</span>
+            </div>
           </div>
         </div>
       </div>
@@ -290,6 +297,40 @@ const Home = () => {
               <a href="#">
                 <span class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
                   H1 HYUNDAI 7 Seater Van
+                </span>
+              </a>
+              <a
+                href="#"
+                class="text-white bg-yellow-400 hover:bg-yellow-600 font-medium rounded-lg text-sm px-3 py-2 text-center flex mt-4 items-center"
+              >
+                Book Now
+                <svg
+                  class="-mr-1 ml-2 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div class="bg-white shadow-md border border-gray-200 rounded-lg  ">
+            <a href="#">
+              <img
+                class="rounded-t-lg"
+                src="https://i.ibb.co/7CNgB2D/hyundai-staria-63d1fe79af681-prev-ui-1.png"
+                alt=""
+              />
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <span class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                  HYUNDAI Staria Van
                 </span>
               </a>
               <a
