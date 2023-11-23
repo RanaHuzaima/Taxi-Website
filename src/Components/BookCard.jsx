3d717f6c6d8a919f3a19962ec7171e90c7c69c32
Rana Huzaima <huzaimadigital@gmail.com>
2023-11-23 16:16:20 +0500

    Make a PickSection component

---
+++ src/Components/BookCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const BookCard = ({ data }) => {
+  return (
+    <>
+      {data.map((data) => (
+        <div class="bg-white shadow-md border border-gray-200 rounded-lg ">
+          <a href="#">
+            <img class="rounded-t-lg" src={data.link} alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <span class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                {data.name}
+              </span>
+            </a>
+            <a
+              href="#"
+              class="text-white bg-yellow-400 hover:bg-yellow-600 font-medium rounded-lg text-sm px-3 py-2 text-center flex mt-4 items-center"
+            >
+              Book Now
+              <svg
+                class="-mr-1 ml-2 h-4 w-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default BookCard;
---
+++ src/Components/PickSection.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import BookCard from "./BookCard";
+
+const PickSection = () => {
+  const cardata = [
+    {
+      link: "https://harmaintransport.com/wp-content/uploads/2020/04/5.png",
+      name: "CAMRY 4 Seater CAR",
+    },
+    {
+      link: "https://harmaintransport.com/wp-content/uploads/2020/04/4.png",
+      name: "INNOVA 7 Seater MPV",
+    },
+    {
+      link: "https://harmaintransport.com/wp-content/uploads/2020/04/3.png",
+      name: "HIACE 10 Seater Van",
+    },
+    {
+      link: "https://harmaintransport.com/wp-content/uploads/2020/04/2.png",
+      name: "GMC 7 Seater SUV",
+    },
+    {
+      link: "https://harmaintransport.com/wp-content/uploads/2023/07/H1-Hyundain-New.png",
+      name: "H1 HYUNDAI 7 Seater Van",
+    },
+    {
+      link: "https://i.ibb.co/7CNgB2D/hyundai-staria-63d1fe79af681-prev-ui-1.png",
+      name: "HYUNDAI Staria Van",
+    },
+  ];
+  return (
+    <>
+      <div className="container mx-auto bg-white mb-12">
+        <div className="flex justify-center mb-14 text-4xl font-extrabold leading-tight lg:text-6xl">
+          <span>
+            Pick Your <span className="text-yellow-400">Umrah</span> Taxi
+          </span>
+        </div>
+        <div class="flex gap-4 lg:flex-nowrap flex-wrap">
+          {/* Book Card */}
+          <BookCard data={cardata} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PickSection;
