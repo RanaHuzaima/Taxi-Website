7fd68aa6180cc806608ec70f781c2f6e5127c021
Rana Huzaima <huzaimadigital@gmail.com>
2023-12-02 18:09:00 +0500

    Make Whatsappicon Responsive

--- src/Components/Whatsappicon.jsx
+++ src/Components/Whatsappicon.jsx
@@ -4,12 +4,10 @@ const Whatsappicon = () => {
   return (
     <>
       <div className="container">
-        <div className="absolute bottom-28 right-28">
+        <div className="absolute bottom-16 right-16 sm:bottom-20 sm:right-20 lg:bottom-28 lg:right-28">
           <a href="https://wa.me/923009464666" target="_blank">
             <img
-              className="fixed"
-              width="94"
-              height="94"
+              className="fixed w-12 md:w-20 lg:w-24"
               src="https://img.icons8.com/3d-fluency/94/whatsapp.png"
               alt="whatsapp"
             />
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
     button: false,
     title: "About Us",
     text: "Mecca Taxi service is a premium transfer-providing company in the Saudi Arabia region. Our high-end and well-maintained vehicles are at your service to meet your transportation requirements whenever you need them. <br/> <br/> Get your ride instantly as you make your ride booking. We have an online booking system to avoid any booking stress. Our booking system is processing 24×7. Our service is reachable easily at airports and hotels and our service is also dedicated to pilgrims. If you are in a search of quick and affordable ride in Saudi then Mecca Taxi Service is what can end your search.",
-    img_link: "https://i.ibb.co/x1hCtGs/World.gif",
+    img_link: "https://i.ibb.co/HYq2Dmq/About-Us.gif",
   };
   return (
     <>
