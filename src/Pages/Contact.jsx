a0f05901399d211bcb5647616f3aba35676e82b0
Rana Huzaima <huzaimadigital@gmail.com>
2023-12-04 18:11:24 +0500

    Make Form Activated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
           </span>
         </div>
 
-        <div className="grid w-full grid-cols-1 my-auto mt-8 mb-8 md:grid-cols-2 lg:grid-cols-3 xl:gap-5 md:gap-5">
+        <div className="grid w-full grid-cols-1 my-auto mt-8 mb-8 md:grid-cols-3 lg:grid-cols-3 xl:gap-5 md:gap-5">
           <div className="p-10 flex flex-col items-center text-center   hover:bg-slate-50 cursor-pointer">
             <span className="p-5 rounded-full bg-yellow-400 text-white shadow-lg shadow-slate-400">
               <svg
@@ -118,39 +118,46 @@ const Contact = () => {
             <span className="text-yellow-400"> Us</span>
           </span>
         </div>
-        <div className="w-full p-8 my-4 md:px-12 mr-auto">
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-5">
-            <input
-              className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="text"
-              placeholder="Name"
-            />
-            <input
-              className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="email"
-              placeholder="Email"
-            />
-            <input
-              className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline number"
-              type="number"
-              placeholder="Phone Number"
-            />
+        <form action="https://formspree.io/f/xoqoogal" method="POST">
+          <div className="w-full p-8 my-4 md:px-12 mr-auto">
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-3 mt-5">
+              <input
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text"
+                name="Name"
+                placeholder="Name"
+                required
+              />
+              <input
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="email"
+                name="Email"
+                placeholder="Email"
+                required
+              />
+              <input
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline number"
+                type="number"
+                name="Phone-Number"
+                placeholder="Phone Number"
+                required
+              />
+            </div>
+            <div className="my-4">
+              <textarea
+                placeholder="Message"
+                name="Message"
+                required
+                className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+              ></textarea>
+            </div>
+            <div className="my-2 flex justify-center items-center">
+              <button className="uppercase text-sm font-bold tracking-wide bg-yellow-400 hover:bg-yellow-600 text-gray-100 p-3 rounded-lg focus:outline-none focus:shadow-outline">
+                Send Message
+              </button>
+            </div>
           </div>
-          <div className="my-4">
-            <textarea
-              placeholder="Message"
-              className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-            ></textarea>
-          </div>
-          <div className="my-2">
-            <button
-              className="uppercase text-sm font-bold tracking-wide bg-yellow-400 text-gray-100 p-3 rounded-lg w-full 
-                      focus:outline-none focus:shadow-outline"
-            >
-              Send Message
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </>
   );
